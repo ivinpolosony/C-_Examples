4cba3340c5600104e6c88f873753bc717855c34e
ivinpolosony <ivinpolosony@gmail.com>
2016-06-26 14:35:11 +0100

    Added file argument to read files

--- csv_read.cc
+++ csv_read.cc
@@ -4,33 +4,50 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <cstdlib> // for exit()
+
 
 using namespace std;
 void display_2dvector( vector< vector<string> >);
 
-int main() 
+int main (int argc, char *argv[])
 { 
-    ifstream filehandle("conf");
-    string str,word; 
-  	stringstream sstr;
-   //  typedef std::pair<int , string> pr; 
-  	// std::vector<pr> v;
-   //  v.push_back(make_pair(1,"sdudfhsdf"));
-   //  std::vector<pr>::iterator it = v.begin();
-   //  cout << it->first << " " << it->second;
-    vector<string> v;
-    vector< vector<string> > vectorofstings;  
-    while (std::getline(filehandle, str))
-    {	   
-      stringstream s(str);
-      while(getline(s,word,',')) 
+      if (argc <= 1)
       {
-            v.push_back(word);
-      } 
-    	vectorofstings.push_back(v);
+        if (argv[0])
+          cout << "Usage: " << argv[0] << " <file name>"  << endl;
+        else
+          cout << "Usage: <program name> <file name>"  << endl;
+        exit(1);
+      }
+      
+      ifstream filehandle(argv[1]);
+      if(filehandle.fail()){
+        cout << "File not found" << endl;
+      }
+      string str,word; 
+      stringstream sstr;
+      //  typedef std::pair<int , string> pr; 
+      // std::vector<pr> v;
+      //  v.push_back(make_pair(1,"sdudfhsdf"));
+      //  std::vector<pr>::iterator it = v.begin();
+      //  cout << it->first << " " << it->second;
+      vector<string> v;
+      vector< vector<string> > vectorofstings;  
+      while (std::getline(filehandle, str))
+      {    
+      stringstream s(str);
+        while(getline(s,word,',')) 
+        {
+        v.push_back(word);
+        } 
+      vectorofstings.push_back(v);
       v.clear();
-    }
-    display_2dvector(vectorofstings);
+      }
+      display_2dvector(vectorofstings);      
+     
+     
+    
     return 0;
 }
 
